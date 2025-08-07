254b3339ee0b05f7e558e768373a30cdf2f83a9f
zohaibsundrali <zohaibhassan6511@gmail.com>
2025-08-07 12:56:39 +0500

    Update files

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,14 +1,15 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import About from "@/components/About";
-import Projects from "@/components/Projects";
+import About from "../about/page";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import Projects from "../components/Projects";
+
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <Hero />
-      <About />
+<Navbar/>
+      <Hero/>
+      <About/>
       <Projects/>
     </>
   );
